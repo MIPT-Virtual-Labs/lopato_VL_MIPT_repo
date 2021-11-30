8fa5cb665f20849cfa0ad1e86810a9a73849579f
Andrey Pikunov <pikunov@phystech.edu>
2021-11-30 16:33:59 +0300

    renaming

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
 
 setup(
-    name="solver_1d_gas_dynamics",
+    name="solver_gas_dynamics_1d",
     version="0.1",
-    packages=["solver_1d_gas_dynamics"],
+    packages=["solver_gas_dynamics_1d"],
     url="https://github.com/MIPT-Virtual-Labs/lopato_VL_MIPT_repo.git",
     author="Aleksander Lopato",
     author_email="lopato2008@mail.ru",
